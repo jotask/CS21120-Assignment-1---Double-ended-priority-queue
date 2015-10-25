2499d712544f531ce19e5564acd1e9e841c78a1f
Jose Vives <jota_sk@hotmail.com>
2015-10-25 10:53:53 +0000

    First commit. Added code provided by the teacher and created the main
    class

---
+++ Double Ended Priority Queue/src/cs21120/depq/Jov2DEPQ.java
@@ -0,0 +1,55 @@
+package cs21120.depq;
+
+public class Jov2DEPQ implements DEPQ{
+	
+	public Jov2DEPQ() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Comparable<?> inspectLeast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparable<?> inspectMost() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(Comparable<?> c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Comparable<?> getLeast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comparable<?> getMost() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
