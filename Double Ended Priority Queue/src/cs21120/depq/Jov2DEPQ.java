29706c52eae40ea944b313d8a34993ea9391d6fd
Jose Vives <jota_sk@hotmail.com>
2015-11-13 15:40:26 +0000

    Added Java Docs and comments for explain what the code does.

--- Double Ended Priority Queue/src/cs21120/depq/Jov2DEPQ.java
+++ Double Ended Priority Queue/src/cs21120/depq/Jov2DEPQ.java
@@ -1,64 +1,44 @@
 package cs21120.depq;
 
+
+/**
+ * Priority Double-Ended Queue assignment
+ *
+ * I implemented a Binary Search Tree because
+ * they have a O(log n) time complexity in average.
+ *
+ */
 public class Jov2DEPQ implements DEPQ{
 
+    /** Root node for the three where all the tree starts. */
     private Node root;
+
+    /** Tree size */
     private int size;
 
+    /**
+     * Constructor for this class.
+     * Just set the root to null and set the size to zero.
+     */
     public Jov2DEPQ() {
         this.root = null;
         this.size = 0;
     }
 
-    private void delete(Comparable element) {
-
-        Node node = root;
-        while (node != null && node.value != null && node.value != element) {
-            if (element.compareTo(node.value) < 0) {
-                node = node.left;
-            } else {
-                node = node.right;
-            }
-        }
-
-        Node deleteNode = node;
-        if (deleteNode == null) {
-            throw new RuntimeException("Something is wrong");
-        }
-        if (deleteNode.left == null) {
-            transfer(deleteNode, deleteNode.right);
-        } else if (deleteNode.right == null) {
-            transfer(deleteNode, deleteNode.left);
-        } else {
-            Node successorNode = deleteNode.right;
-            while (successorNode.left != null) {
-                successorNode = successorNode.left;
-            }
-            if (successorNode.parent != deleteNode) {
-                transfer(successorNode, successorNode.right);
-                successorNode.right = deleteNode.right;
-                successorNode.right.parent = successorNode;
-            }
-            transfer(deleteNode, successorNode);
-            successorNode.left = deleteNode.left;
-            successorNode.left.parent = successorNode;
-        }
-        size--;
-    }
-
-    private void transfer(Node nodeToReplace, Node newNode) {
-        if (nodeToReplace.parent == null) {
-            this.root = newNode;
-        } else if (nodeToReplace == nodeToReplace.parent.left) {
-            nodeToReplace.parent.left = newNode;
-        } else {
-            nodeToReplace.parent.right = newNode;
-        }
-        if (newNode != null) {
-            newNode.parent = nodeToReplace.parent;
-        }
-    }
-
+    /**
+     * Inspect the node with the least value.
+     * Basically keep looking for the bottom left node
+     * until his left child is null. That mean, that
+     * node is the node with the least value.
+     * This method does not remove the value from the tree
+     *
+     * Time Complexity
+     *      Average: O(log n)
+     *      Worst Case: O(n)
+     *
+     * @return
+     *      The comparable with the least node.
+     */
     @Override
     public Comparable inspectLeast() {
         Node node = root;
@@ -68,6 +48,20 @@ public class Jov2DEPQ implements DEPQ{
         return node.value;
     }
 
+    /**
+     * Inspect the node with the most value.
+     * Basically keep looking for the bottom right node
+     * until his right child is null. That mean, that
+     * node is the node with the most value.
+     * This method does not remove the value from the tree
+     *
+     * Time Complexity
+     *      Average: O(log n)
+     *      Worst Case: O(n)
+     *
+     * @return
+     *      The comparable with the most node.
+     */
     @Override
     public Comparable inspectMost() {
         Node node = root;
@@ -77,15 +71,36 @@ public class Jov2DEPQ implements DEPQ{
         return node.value;
     }
 
+    /**
+     * Insert a new value to the tree
+     *
+     * Time Complexity
+     *      Average: O(log n)
+     *      Worst Case: O(n)
+     *
+     * @param value
+     *      The value we want insert
+     */
     @Override
     public void add(Comparable value) {
+
+        // If the root node is empty, means that the tree is null
+        // Create a new node with the value, to be the root node
+        // increment the size and exit the methods, we done.
         if (root == null) {
             root = new Node(value);
             size++;
             return;
         }
+
+        // Create two node instance for the searching
         Node insertParentNode = null;
         Node searchTempNode = root;
+
+        // Keep looking until we reach a node with out
+        // child, deciding if we go to the left child
+        // or the right child depending on the value we
+        // want insert to the tree
         while (searchTempNode != null && searchTempNode.value != null) {
             insertParentNode = searchTempNode;
             if (value.compareTo(searchTempNode.value) < 0) {
@@ -94,16 +109,38 @@ public class Jov2DEPQ implements DEPQ{
                 searchTempNode = searchTempNode.right;
             }
         }
+
+        // Create a new node to insert with the value we want add
+        // and set the parent to the parent we search before.
         Node newNode = new Node(value, insertParentNode);
+
+        // Check if the node for insert is not null
+        // If is null throw an assert exception
         assert insertParentNode != null;
+
+        // Check the value of the node we want to add to this parent node.
+        // If the value is more thant the parent value means this new node
+        // go to the right, if not, go to the left
         if (insertParentNode.value.compareTo(newNode.value) > 0) {
             insertParentNode.left = newNode;
         } else {
             insertParentNode.right = newNode;
         }
+
+        // Increment the size by one
         size++;
     }
 
+    /**
+     * Get the least value from the tree and remove from the tree
+     *
+     * Time Complexity
+     *      Average: O(log n)
+     *      Worst Case: O(n)
+     *
+     * @return
+     *      The least value from the tree
+     */
     @Override
     public Comparable getLeast() {
         Comparable least = inspectLeast();
@@ -112,6 +149,16 @@ public class Jov2DEPQ implements DEPQ{
 
     }
 
+    /**
+     * Get the most value from the tree and remove from the tree
+     *
+     * Time Complexity
+     *      Average: O(log n)
+     *      Worst Case: O(n)
+     *
+     * @return
+     *      The most value from the tree
+     */
     @Override
     public Comparable getMost() {
         Comparable most = inspectMost();
@@ -119,27 +166,169 @@ public class Jov2DEPQ implements DEPQ{
         return most;
     }
 
+    /**
+     * Know if the tree is empty
+     * @return
+     *      if is empty
+     */
     @Override
     public boolean isEmpty() {
         return (size() == 0);
     }
 
+    /**
+     * Know the size of the tree
+     * @return
+     *      the size of the tree
+     */
     @Override
     public int size() {
         return size;
     }
 
+    /**
+     * Remove the value we want if exist in out tree.
+     *
+     * Time complexity:
+     *      Average: O(log n)
+     *      Worst case: O(n)
+     *
+     * @param element
+     *          The value needs to be deleted
+     */
+    private void delete(Comparable element) {
+
+        // Search the node which have the value we want delete. We start
+        // from the root of the tree and keep searching until we found the value
+        Node deleteNode = root;
+        while (deleteNode != null && deleteNode.value != null && deleteNode.value != element) {
+            if (element.compareTo(deleteNode.value) < 0) {
+                deleteNode = deleteNode.left;
+            } else {
+                deleteNode = deleteNode.right;
+            }
+        }
+
+        // Last check before continue.
+        // Make sure the node we want delete is not null, and have the same value we want delete
+        // If not throw an exception. This can be improve on the interface to set to some methods
+        // the possibility to throw exceptions for handle it the problems.
+        if (deleteNode == null || !deleteNode.value.equals(element)) {
+            throw new RuntimeException("Value not found.");
+        }
+
+        // Delete the node by change the references to the other nodes. The Garbage collector
+        // just delete the node without reference
+        if (deleteNode.left == null) {
+            // Does not have any left child
+            // set this node to be the right child
+            transfer(deleteNode, deleteNode.right);
+        } else if (deleteNode.right == null) {
+            // Does not have a right child
+            // Set this node to be the left child
+            transfer(deleteNode, deleteNode.left);
+        } else {
+
+            // If have left child and a right we need to deal with his children's
+            // Create a instance of the right child
+            Node successorNode = deleteNode.right;
+
+            // Keep looking for the right less child with the least node
+            while (successorNode.left != null) {
+                successorNode = successorNode.left;
+            }
+
+            // If the parent of the node found is not the node we want delete
+            // transfer the parent of the successorNOde, to his right child.
+            // Set the right child of the successor node, to the right child from
+            // the node we want delete. Change right child parent to the successor
+            // node
+            if (successorNode.parent != deleteNode) {
+                transfer(successorNode, successorNode.right);
+                successorNode.right = deleteNode.right;
+                successorNode.right.parent = successorNode;
+            }
+
+            // Transfer the node we want delete to the successor node we founded, later
+            // set left child a reference to the left child of the node we want delete,
+            // and change his left child parent reference to be the successor node we
+            // founds.
+            transfer(deleteNode, successorNode);
+            successorNode.left = deleteNode.left;
+            successorNode.left.parent = successorNode;
+        }
+
+        // Decrease the size by one
+        size--;
+    }
+
+    /**
+     * Transfer one node from tree to the place of another node
+     * @param nodeToReplace
+     *      Node which is go to be replace by the other
+     *      node anb removed from the tree
+     * @param newNode
+     *      The new node to be replaced
+     */
+    private void transfer(Node nodeToReplace, Node newNode) {
+        if (nodeToReplace.parent == null) {
+            // Check if the parent of this node is null.
+            // If is null means the nodeToReplace is the root node
+            // Set the newNode to be the new root node
+            this.root = newNode;
+        } else if (nodeToReplace == nodeToReplace.parent.left) {
+            //If the node we want replace is the left child from his parent
+            // is because have a lowest value change the parent less child
+            // to be the new node
+            nodeToReplace.parent.left = newNode;
+        } else {
+            // If not means is the right value, so set the parent right node
+            // from the node we want replace to be the new node
+            nodeToReplace.parent.right = newNode;
+        }
+        // If the new node is not null
+        // set the parent of the new node to be the parent
+        // of the node we want replace
+        if (newNode != null) {
+            newNode.parent = nodeToReplace.parent;
+        }
+    }
+
+    /**
+     * Inner class Node
+     * This class hold the value for a node, his parent instance and his left
+     * and right child instance
+     */
     public class Node {
 
-        public final Comparable value;
-        public Node parent;
-        public Node left;
-        public Node right;
+        /** The comparable value for this node */
+        final Comparable value;
 
+        /** His node parent */
+        Node parent;
+
+        /** His left child node */
+        Node left;
+
+        /** His right child */
+        Node right;
+
+        /**
+         * Constructor with only passing the comparable value
+         * @param value
+         *      The comparable value for this node instance
+         */
         public Node(Comparable value) {
             this(value, null);
         }
 
+        /**
+         * Constructor passing a comparable value and his parent
+         * @param value
+         *      The comparable value for this node instance
+         * @param parent
+         *      The parent for this node
+         */
         public Node(Comparable value, Node parent) {
             this.value = value;
             this.parent = parent;
@@ -148,4 +337,6 @@ public class Jov2DEPQ implements DEPQ{
         }
 
     }
+    // End class Node
+
 }
